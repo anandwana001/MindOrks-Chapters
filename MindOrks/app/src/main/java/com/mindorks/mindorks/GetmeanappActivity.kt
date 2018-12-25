b539c3f0f8e8ba3d5b641d7a836a89b4ecf4c8af
Akshay Nandwana <akshaynandwana@Akshays-MacBook-Pro.local>
2018-12-25 16:57:39 +0530

    Add Activity Lifecycle Project

---
+++ MindOrks/app/src/main/java/com/mindorks/mindorks/GetmeanappActivity.kt
@@ -0,0 +1,16 @@
+package com.mindorks.mindorks
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by akshaynandwana on
+ * 24, December, 2018
+ **/
+class GetmeanappActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_getmeanapp)
+    }
+}
